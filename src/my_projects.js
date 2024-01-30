8bd4f47d4a080018c2e59bc3cad2538e016390c0
Grant Burke <85257846+1grantaburke1@users.noreply.github.com>
2024-01-30 08:00:49 -0800

    Add files via upload

---
+++ src/my_projects.js
@@ -0,0 +1,34 @@
+let RBCSection = document.querySelector(".RBC_container");
+let RBCInfo = RBCSection.querySelector(".information");
+let RBCHeader = RBCSection.querySelector(".information > h2 a");
+
+let RBCMarker = RBCSection.querySelector(".information .marker");
+let hrMarker = RBCSection.querySelector(".information hr");
+
+let removableParas = RBCSection.querySelectorAll(".information .removable");
+
+if (window.innerWidth <= 980) {
+    RBCHeader.style.textDecoration = "none";
+    RBCHeader.style.pointerEvents = "none";
+    RBCHeader.style.cursor = "default";
+
+    removableParas.forEach((para) => {
+        RBCInfo.removeChild(para);
+    });
+
+    let newPara = document.createElement("p");
+    let newBold = document.createElement("b");
+
+    newBold.textContent = "Available to View?: ";
+    newPara.appendChild(newBold);
+    newPara.append("No");
+    RBCMarker.before(newPara);
+
+    newPara = document.createElement("p");
+    newBold = document.createElement("b");
+
+    newBold.textContent = "Link to webpage: ";
+    newPara.appendChild(newBold);
+    newPara.append("Unavailable on Mobile");
+    RBCMarker.after(newPara);
+}
