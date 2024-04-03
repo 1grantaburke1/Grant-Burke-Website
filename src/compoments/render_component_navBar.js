8a550bf6488d837f4135afb6a96a39b763d50401
Grant Burke <85257846+1grantaburke1@users.noreply.github.com>
2024-04-03 09:28:28 -0700

    Update render_component_navBar.js

--- src/compoments/render_component_navBar.js
+++ src/compoments/render_component_navBar.js
@@ -7,11 +7,11 @@ document.write(`
 </a>
 <ul class=navigation>
       <li><a href="https://grantaburke.com"><i class="fa-solid fa-house"></i> Home</a></li>
-      <!-- <li><a href="./my_story.html">My Story</a></li> -->
+      <!-- <li><a href="./my_story">My Story</a></li> -->
       <li><a href="./work_experience"><i class="fa-solid fa-briefcase"></i> Work Experience</a></li>
-      <li><a href="./my_projects.html"><i class="fa-solid fa-code"></i> My Projects</a></li>
-      <li><a href="./FAQ.html"><i class="fa-solid fa-circle-question"></i> FAQs</a></li>
-      <li><a href="./contact_information.html"><i class="fa-solid fa-phone"></i> Contact Me</a></li>
+      <li><a href="./my_projects"><i class="fa-solid fa-code"></i> My Projects</a></li>
+      <li><a href="./FAQ"><i class="fa-solid fa-circle-question"></i> FAQs</a></li>
+      <li><a href="./contact_information"><i class="fa-solid fa-phone"></i> Contact Me</a></li>
 </ul>
 </nav>
 <button class="toggle_nav">
