f16f36c51efd735ff23518166f9c83870ba317e4
Grant Burke <85257846+1grantaburke1@users.noreply.github.com>
2024-04-03 09:27:37 -0700

    Add files via upload

---
+++ src/compoments/render_component_navBar.js
@@ -0,0 +1,27 @@
+document.write(`
+
+<nav class="navbar">
+<a href="https://grantaburke.com" class="logo">
+      <img alt="" width="90">
+      Grant Burke
+</a>
+<ul class=navigation>
+      <li><a href="https://grantaburke.com"><i class="fa-solid fa-house"></i> Home</a></li>
+      <!-- <li><a href="./my_story.html">My Story</a></li> -->
+      <li><a href="./work_experience"><i class="fa-solid fa-briefcase"></i> Work Experience</a></li>
+      <li><a href="./my_projects.html"><i class="fa-solid fa-code"></i> My Projects</a></li>
+      <li><a href="./FAQ.html"><i class="fa-solid fa-circle-question"></i> FAQs</a></li>
+      <li><a href="./contact_information.html"><i class="fa-solid fa-phone"></i> Contact Me</a></li>
+</ul>
+</nav>
+<button class="toggle_nav">
+<p>&uarr;</p>
+<div class="hamburger_btn">
+      <hr>
+      <hr>
+      <hr>
+</div>
+</button>
+<div class="empty"></div>
+
+`);
