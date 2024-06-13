b5aef0ec6fb440e626b34e9948dc6696fc22939b
Grant Burke <85257846+1grantaburke1@users.noreply.github.com>
2024-06-13 11:33:46 -0700

    Update render_component_navBar.js

--- src/compoments/render_component_navBar.js
+++ src/compoments/render_component_navBar.js
@@ -1,17 +1,17 @@
 document.write(`
 
 <nav class="navbar">
-<a href="https://grantaburke.com" class="logo">
+<a href="./index.html" class="logo">
       <img alt="" width="90">
       Grant Burke
 </a>
 <ul class=navigation>
-      <li><a href="https://grantaburke.com"><i class="fa-solid fa-house"></i> Home</a></li>
-      <!-- <li><a href="./my_story">My Story</a></li> -->
-      <li><a href="./work_experience"><i class="fa-solid fa-briefcase"></i> Work Experience</a></li>
-      <li><a href="./my_projects"><i class="fa-solid fa-code"></i> My Projects</a></li>
-      <li><a href="./FAQs"><i class="fa-solid fa-circle-question"></i> FAQs</a></li>
-      <li><a href="./contact_information"><i class="fa-solid fa-phone"></i> Contact Me</a></li>
+      <li><a href="./index.html"><i class="fa-solid fa-house"></i> Home</a></li>
+      <!-- <li><a href="./my_story.html">My Story</a></li> -->
+      <li><a href="./work_experience.html"><i class="fa-solid fa-briefcase"></i> Work Experience</a></li>
+      <li><a href="./my_projects.html"><i class="fa-solid fa-code"></i> My Projects</a></li>
+      <li><a href="./FAQs.html"><i class="fa-solid fa-circle-question"></i> FAQs</a></li>
+      <li><a href="./contact_information.html"><i class="fa-solid fa-phone"></i> Contact Me</a></li>
 </ul>
 </nav>
 <button class="toggle_nav">
@@ -22,6 +22,12 @@ document.write(`
       <hr>
 </div>
 </button>
-<div class="empty"></div>
 
 `);
+
+//Add empty space
+if (this.innerWidth > 974) {
+    document.write(`
+      <div class="empty"></div>
+      `);
+}
